e8043808255aad9bd9e1ab6b55299bcd6c044264
Peter Donald <peter@realityforge.org>
2016-12-02 17:27:55 +1100

    Rename test

--- test/generators/test_template.rb
+++ test/generators/test_template.rb
@@ -1,6 +1,6 @@
 require File.expand_path('../../helper', __FILE__)
 
-class Reality::Generators::TestTargetManager < Reality::TestCase
+class Reality::Generators::TestTemplate < Reality::TestCase
   module MyHelper
     def a
       'A'
