82377d56919453f2263eb6c42d894e5603eea627
Peter Donald <peter@realityforge.org>
2016-12-02 17:28:22 +1100

    Add a basic ruby template

---
+++ test/generators/test_ruby_template.rb
@@ -0,0 +1,46 @@
+require File.expand_path('../../helper', __FILE__)
+
+class Reality::Generators::TestRubyTemplate < Reality::TestCase
+
+  class SimpleModel
+    def name
+      'SimpleModel'
+    end
+
+    def facet_enabled?(facet)
+      true
+    end
+  end
+
+  def test_ruby_template
+    template_set = Reality::Generators::TemplateSet.new('foo')
+
+    output_filename_pattern = 'main/java/#{component.name}.java'
+    template_filename = File.expand_path(File.dirname(__FILE__) + '/templates/rubytemplate.rb')
+    Reality::Generators::TargetManager.target(:component)
+
+    template1 = Reality::Generators::RubyTemplate.new(template_set, [], :component, template_filename, output_filename_pattern, [], {})
+
+    assert_equal output_filename_pattern, template1.output_filename_pattern
+    assert_equal output_filename_pattern, template1.output_path
+    assert_equal template_set, template1.template_set
+    assert_equal [], template1.facets
+    assert_equal :component, template1.target
+    assert_equal [], template1.helpers
+    assert_equal template_filename, template1.template_key
+    assert_equal nil, template1.guard
+    assert_equal({}, template1.extra_data)
+    assert_equal 'foo:rubytemplate', template1.name
+
+    target_basedir = "#{temp_dir}/generated/ruby_template"
+    target_filename = "#{target_basedir}/main/java/SimpleModel.java"
+    other_filename = "#{target_basedir}/main/java/Other.java"
+    unprocessed_files = %W(#{target_filename} #{other_filename})
+    assert_equal false, File.exist?(target_filename)
+    template1.generate(target_basedir, SimpleModel.new, unprocessed_files)
+    assert_equal true, File.exist?(target_filename)
+    assert_equal 1, unprocessed_files.size
+
+    assert_equal 'Here is a SimpleModel', IO.read(target_filename)
+  end
+end
